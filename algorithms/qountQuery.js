2ce4a7fd2a4b27a5d54210670b2798bf22da2373
Tito <92267788+titosunu@users.noreply.github.com>
2024-07-23 12:05:19 +0700

    Delete algorithms directory

--- algorithms/qountQuery.js
+++
@@ -1,9 +0,0 @@
-// Soal Algorithms 3
-const input = ["xc", "dz", "bbb", "dz"];
-const query = ["bbb", "ac", "dz"];
-
-function countQuery() {
-  return query.map((query) => input.filter((item) => item === query).length);
-}
-
-console.log(countQuery());
